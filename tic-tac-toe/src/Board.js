f7de12d0eb27eb4da32e93b6edd5d49c996cbd3b
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-19 15:41:38 -0300

    adição do histórico de jogo

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -1,77 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Square from "./Square";
 
 function Board(props) {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, SetXIsNext] = useState(true);
-  const [status, setStatus] = useState("Next player: X");
-
-  function handleClick(i) {
-    const updatedSquares = squares.slice();
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    updatedSquares[i] = xIsNext ? "X" : "O";
-
-    setSquares(updatedSquares);
-    SetXIsNext(!xIsNext);
-  }
-
-  useEffect(() => {
-    const winner = calculateWinner(squares);
-
-    if (winner) {
-      setStatus(`Winner: ${winner}`);
-    } else {
-      setStatus(`Next player: ${xIsNext ? "X" : "O"}`);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [squares]);
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
   return (
     <>
-      <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onClick={() => handleClick(0)}></Square>
-        <Square value={squares[1]} onClick={() => handleClick(1)}></Square>
-        <Square value={squares[2]} onClick={() => handleClick(2)}></Square>
+        <Square
+          value={props.squares[0]}
+          onClick={() => props.onClick(0)}
+        ></Square>
+        <Square
+          value={props.squares[1]}
+          onClick={() => props.onClick(1)}
+        ></Square>
+        <Square
+          value={props.squares[2]}
+          onClick={() => props.onClick(2)}
+        ></Square>
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onClick={() => handleClick(3)}></Square>
-        <Square value={squares[4]} onClick={() => handleClick(4)}></Square>
-        <Square value={squares[5]} onClick={() => handleClick(5)}></Square>
+        <Square
+          value={props.squares[3]}
+          onClick={() => props.onClick(3)}
+        ></Square>
+        <Square
+          value={props.squares[4]}
+          onClick={() => props.onClick(4)}
+        ></Square>
+        <Square
+          value={props.squares[5]}
+          onClick={() => props.onClick(5)}
+        ></Square>
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onClick={() => handleClick(6)}></Square>
-        <Square value={squares[7]} onClick={() => handleClick(7)}></Square>
-        <Square value={squares[8]} onClick={() => handleClick(8)}></Square>
+        <Square
+          value={props.squares[6]}
+          onClick={() => props.onClick(6)}
+        ></Square>
+        <Square
+          value={props.squares[7]}
+          onClick={() => props.onClick(7)}
+        ></Square>
+        <Square
+          value={props.squares[8]}
+          onClick={() => props.onClick(8)}
+        ></Square>
       </div>
     </>
   );
--- tic-tac-toe/src/Game.js
+++ tic-tac-toe/src/Game.js
@@ -1,15 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Board from "./Board";
 
 function Game(props) {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [current, SetCurrent] = useState({ squares: Array(9).fill(null) });
+  const [xIsNext, SetXIsNext] = useState(true);
+  const [status, setStatus] = useState("Next player: X");
+
+  function handleClick(i) {
+    const historyCurrent = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = xIsNext ? "X" : "O";
+
+    historyCurrent.squares = squares;
+
+    SetCurrent(historyCurrent);
+    setHistory([...history]);
+    SetXIsNext(!xIsNext);
+  }
+
+  useEffect(() => {
+    const historyCurrent = history[history.length - 1];
+    const squares = historyCurrent.squares.slice();
+
+    const winner = calculateWinner(squares);
+
+    if (winner) {
+      setStatus(`Winner: ${winner}`);
+    } else {
+      setStatus(`Next player: ${xIsNext ? "X" : "O"}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [history]);
+
+  useEffect(() => {
+    SetCurrent(history[history.length - 1]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={i => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
